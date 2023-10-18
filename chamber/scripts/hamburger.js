cb4704e807495d50a1da23a922613d8716959796
Matthew Knorr <matt@knorrs.net>
2023-10-17 22:26:51 -0600

    Bug Fixes final mobile form

--- chamber/scripts/hamburger.js
+++ chamber/scripts/hamburger.js
@@ -1,8 +1,11 @@
 const hamburgerButton = document.getElementById("hamburger-menu");
 const mobileMenu = document.getElementById("mobile-menu");
+const joinSection = document.querySelector(".join"); // Get the .join element
 
 // Add a click event listener to the button
 hamburgerButton.addEventListener("click", () => {
     // Toggle the visibility of the mobile menu by toggling the "show-menu" class
     mobileMenu.classList.toggle("show-menu");
+
+    joinSection.classList.toggle("push-down");
 });
