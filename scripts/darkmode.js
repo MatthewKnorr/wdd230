66ebdf4a45ac272cdf7259e7fbddd1314852fd93
Matthew Knorr <matt@knorrs.net>
2023-10-21 21:36:03 -0600

    bug fixes

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -1,22 +1,21 @@
 const darkModeButton = document.getElementById("darkModeButton");
     const body = document.body;
 
-    // Check if dark mode is set in localStorage and apply it on page load
     const isDarkMode = localStorage.getItem("darkMode") === "true";
     if (isDarkMode) {
         body.classList.add("dark-mode");
-        darkModeButton.textContent = "🌙";
+        darkModeButton.textContent = "🌑";
     }
 
     darkModeButton.addEventListener("click", () => {
         body.classList.toggle("dark-mode");
 
-        // Toggle the emoji
+        // Toggle  emoji
         if (body.classList.contains("dark-mode")) {
             darkModeButton.textContent = "☀️";
-            localStorage.setItem("darkMode", "false"); // Save light mode preference
+            localStorage.setItem("darkMode", "false"); // Save light mode 
         } else {
-            darkModeButton.textContent = "🌙";
-            localStorage.setItem("darkMode", "true"); // Save dark mode preference
+            darkModeButton.textContent = "🌑";
+            localStorage.setItem("darkMode", "true"); // Save dark mode 
         }
     });
