b458279904e5421b62043dd311b535d25d41ade1
Matthew Knorr <matt@knorrs.net>
2023-11-10 01:55:56 -0700

    bug fixes

--- scripts/links.js
+++ scripts/links.js
@@ -1,6 +1,6 @@
 const baseURL = "https://matthewknorr.github.io/wdd230/";
 const linksURL = "https://matthewknorr.github.io/wdd230/data/links.json";
-const phrasesURL = "https://matthewknorr.github.io/wdd230/data/phrases.json"; // Add the URL for phrases.json
+const phrasesURL = "https://matthewknorr.github.io/wdd230/data/phrases.json";
 
 async function getLinks() {
   try {
@@ -16,14 +16,32 @@ async function getPhrases() {
   try {
     const response = await fetch(phrasesURL);
     const data = await response.json();
-    updateListWithRandomPhrase(data);
+    return data;
   } catch (error) {
     console.error('Error fetching phrases data:', error);
   }
 }
 
-getLinks();
-getPhrases(); // Call getPhrases to fetch and update phrases
+function getRandomPhrase(phrases) {
+  const randomIndex = Math.floor(Math.random() * phrases.length);
+  return phrases[randomIndex];
+}
+
+async function updateListWithRandomPhrase() {
+  const phrasesData = await getPhrases();
+  const listElement = document.querySelector(".card-one ol");
+
+  const listItem = document.createElement("li");
+  const unorderedList = document.createElement("ul");
+  const phraseItem = document.createElement("li");
+
+  const phrase = getRandomPhrase(phrasesData.phrases);
+  phraseItem.textContent = phrase;
+
+  unorderedList.appendChild(phraseItem);
+  listItem.appendChild(unorderedList);
+  listElement.appendChild(listItem);
+}
 
 function displayLinks(weeks) {
   const cardOne = document.querySelector('.card-one ol');
@@ -32,25 +50,16 @@ function displayLinks(weeks) {
     week.links.forEach((link) => {
       const listItem = document.createElement('li');
       const anchor = document.createElement('a');
+
       anchor.href = link.url;
       anchor.textContent = link.title;
+
       listItem.appendChild(anchor);
       cardOne.appendChild(listItem);
     });
   });
+
+  updateListWithRandomPhrase();
 }
 
-function getRandomPhrase(phrases) {
-  const randomIndex = Math.floor(Math.random() * phrases.length);
-  return phrases[randomIndex];
-}
-
-function updateListWithRandomPhrase(phrasesData) {
-  const listElement = document.querySelector(".card-one ol"); // Assuming you want to add the phrase to the same list
-  const phrase = getRandomPhrase(phrasesData.phrases);
-
-  const listItem = document.createElement("li");
-  listItem.textContent = phrase;
-
-  listElement.appendChild(listItem);
-}
+getLinks();
