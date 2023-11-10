37ba570f5ff68ac0c1c2ee7e087927d8610f5262
Matthew Knorr <matt@knorrs.net>
2023-11-10 01:44:08 -0700

    bug fixes

--- scripts/links.js
+++ scripts/links.js
@@ -1,46 +1,56 @@
 const baseURL = "https://matthewknorr.github.io/wdd230/";
-
-// Define the URL for the links.json data file
 const linksURL = "https://matthewknorr.github.io/wdd230/data/links.json";
+const phrasesURL = "https://matthewknorr.github.io/wdd230/data/phrases.json"; // Add the URL for phrases.json
 
-// Function to get links data
 async function getLinks() {
   try {
     const response = await fetch(linksURL);
     const data = await response.json();
-    displayLinks(data); // Call the displayLinks function with the links data
+    displayLinks(data);
   } catch (error) {
     console.error('Error fetching links data:', error);
   }
 }
 
-// Call the getLinks function to start fetching and processing the data
+async function getPhrases() {
+  try {
+    const response = await fetch(phrasesURL);
+    const data = await response.json();
+    updateListWithRandomPhrase(data);
+  } catch (error) {
+    console.error('Error fetching phrases data:', error);
+  }
+}
+
 getLinks();
+getPhrases(); // Call getPhrases to fetch and update phrases
 
-// Function to display links
 function displayLinks(weeks) {
-  const cardContainer = document.querySelector('.card-container');
-  const learningActivitiesList = document.querySelector('.card-one ol');
+  const cardOne = document.querySelector('.card-one ol');
 
-  // Iterate through each week in the data
   weeks.lessons.forEach((week) => {
-    // Create a list item for each link in the week
     week.links.forEach((link) => {
       const listItem = document.createElement('li');
       const anchor = document.createElement('a');
-
-      // Set the href and text content of the anchor element
       anchor.href = link.url;
       anchor.textContent = link.title;
-
-      // Append the anchor element to the list item
       listItem.appendChild(anchor);
-
-      // Append the list item to the learning activities list
-      learningActivitiesList.appendChild(listItem);
+      cardOne.appendChild(listItem);
     });
   });
+}
 
-  // Append the updated learning activities list to the card container
-  cardContainer.appendChild(learningActivitiesList);
+function getRandomPhrase(phrases) {
+  const randomIndex = Math.floor(Math.random() * phrases.length);
+  return phrases[randomIndex];
+}
+
+function updateListWithRandomPhrase(phrasesData) {
+  const listElement = document.querySelector(".card-one ol"); // Assuming you want to add the phrase to the same list
+  const phrase = getRandomPhrase(phrasesData.phrases);
+
+  const listItem = document.createElement("li");
+  listItem.textContent = phrase;
+
+  listElement.appendChild(listItem);
 }
