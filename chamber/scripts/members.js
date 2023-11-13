916a8bdfbce09a4bfb1226421126d2f4916232d2
Matthew Knorr <matt@knorrs.net>
2023-11-12 20:13:49 -0700

    member fix

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,7 +1,7 @@
-// Update the const variable named "url" with the URL string of the new JSON resource.
+// Update the const variable named "membersUrl" with the URL string of the new JSON resource.
 const membersUrl = 'https://raw.githubusercontent.com/MatthewKnorr/wdd230/main/chamber/data/members.json';
 
-// Update the const variable named "members" to select the HTML div element from the document with an id value of "members".
+// Update the const variable named "membersSection" to select the HTML div element from the document with an id value of "members".
 const membersSection = document.querySelector('#members');
 
 // Create an async defined function named "getMembersData" to fetch data from the new JSON source url.
@@ -26,22 +26,43 @@ const displayMembers = (members) => {
         // Create elements to add to the div.members element
         let memberCard = document.createElement('section');
         let memberName = document.createElement('h2');
+        let memberAddress = document.createElement('p');
+        let memberPhone = document.createElement('p');
+        let memberWebsite = document.createElement('a');
         let memberImage = document.createElement('img');
+        let membershipLevel = document.createElement('p');
+        let otherInfo = document.createElement('p');
 
-        // Build the h2 content to show the member's name
+        // Add specific classes to the elements
+        memberCard.classList.add('js-section');
+        memberName.classList.add('js-h2');
+        memberImage.classList.add('js-img');
+
+        // Build the content for each element
         memberName.textContent = member.name;
-
-        // Build the image by setting attributes
+        memberAddress.textContent = `${member.address}`;
+        memberPhone.textContent = `${member.phone}`;
+        memberWebsite.textContent = `${member.website}`;
+        memberWebsite.href = member.website; // Set the href attribute for the link
+        memberWebsite.target = "_blank"; // Open the link in a new tab
         memberImage.setAttribute('src', member.image);
         memberImage.setAttribute('alt', `Image of ${member.name}`);
         memberImage.setAttribute('loading', 'lazy');
         memberImage.setAttribute('width', '340');
         memberImage.setAttribute('height', '440');
+        membershipLevel.textContent = `Membership Level: ${member.membershipLevel}`;
+        otherInfo.textContent = `${member.otherInfo}`;
 
-        // Append the section (member card) with the created elements
+        // Append the elements to the memberCard
         memberCard.appendChild(memberName);
+        memberCard.appendChild(memberAddress);
+        memberCard.appendChild(memberPhone);
+        memberCard.appendChild(memberWebsite);
         memberCard.appendChild(memberImage);
+        memberCard.appendChild(membershipLevel);
+        memberCard.appendChild(otherInfo);
 
+        // Append the memberCard to the #members section
         membersSection.appendChild(memberCard);
     });
 }
