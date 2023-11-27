43008586fb48fea7ae13907f7d5f4a692a919fe6
Matthew Knorr <matt@knorrs.net>
2023-11-27 02:11:14 -0700

    bug fix

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -21,6 +21,27 @@ const updateView = () => {
 
     // Display an emoji based on the view mode
     toggleViewButton.textContent = isGridView ? '📷 Grid View' : '📝 List View';
+
+    // Update image size and text layout based on the view mode
+    const cards = document.querySelectorAll('.js-section');
+    cards.forEach((card) => {
+        const image = card.querySelector('.js-img');
+        const textContainer = card.querySelector('.text-container');
+
+        if (isGridView) {
+            // Set styles for grid view
+            image.setAttribute('width', '340');
+            image.setAttribute('height', '440');
+            textContainer.style.flexDirection = 'column';
+            textContainer.style.whiteSpace = 'normal'; // Allow text to wrap in grid view
+        } else {
+            // Set styles for list view
+            image.setAttribute('width', '60');
+            image.setAttribute('height', '80');
+            textContainer.style.flexDirection = 'row';
+            textContainer.style.whiteSpace = 'normal'; // Allow text to wrap in list view
+        }
+    });
 };
 
 // Async function to fetch and display members
@@ -39,6 +60,12 @@ const displayMembers = (members) => {
     members.forEach((member) => {
         // Create elements to add to the div.members element
         let memberCard = document.createElement('section');
+        memberCard.classList.add('js-section');
+
+        // Create a div to contain text elements
+        let textContainer = document.createElement('div');
+        textContainer.classList.add('text-container');
+
         let memberName = document.createElement('h2');
         let memberAddress = document.createElement('p');
         let memberPhone = document.createElement('p');
@@ -48,7 +75,6 @@ const displayMembers = (members) => {
         let otherInfo = document.createElement('p');
 
         // Add specific classes to the elements
-        memberCard.classList.add('js-section');
         memberName.classList.add('js-h2');
         memberImage.classList.add('js-img');
 
@@ -67,14 +93,17 @@ const displayMembers = (members) => {
         membershipLevel.textContent = `Membership Level: ${member.membershipLevel}`;
         otherInfo.textContent = `${member.otherInfo}`;
 
-        // Append the elements to the memberCard
-        memberCard.appendChild(memberName);
-        memberCard.appendChild(memberAddress);
-        memberCard.appendChild(memberPhone);
-        memberCard.appendChild(memberWebsite);
+        // Append the elements to the textContainer
+        textContainer.appendChild(memberName);
+        textContainer.appendChild(memberAddress);
+        textContainer.appendChild(memberPhone);
+        textContainer.appendChild(memberWebsite);
+        textContainer.appendChild(membershipLevel);
+        textContainer.appendChild(otherInfo);
+
+        // Append the textContainer and memberImage to the memberCard
+        memberCard.appendChild(textContainer);
         memberCard.appendChild(memberImage);
-        memberCard.appendChild(membershipLevel);
-        memberCard.appendChild(otherInfo);
 
         // Append the memberCard to the #members section
         membersSection.appendChild(memberCard);
